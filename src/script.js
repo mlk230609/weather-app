baf246e4b33eb023424c6a0d844ca1a256f6b677
mlk230609 <102431840+mlk230609@users.noreply.github.com>
2022-08-21 11:42:53 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,103 +1,83 @@
-body {
-  font-family: "Courier New", Courier, monospace;
-  margin: 0;
-  padding: 0;
-  background-color: #fff;
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-button .button-location {
-  cursor: pointer;
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function searchCity(city) {
+  let apiKey = "217c7d93315f0291dafff5c31e0a37d2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "217c7d93315f0291dafff5c31e0a37d2";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+    position.coords.latitude
+  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-input {
-  -webkit-appearance: none;
-}
-.button-location{
-  border:#7A6CC1;
-  background: #0D6EFD;
-  color: inherit;
-  display: block;
-
-}
-button,
-input  {
-  border:#7A6CC1;
-  background: plum;
-  color: inherit;
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-.container {
-  font-family: 'Grape Nuts', cursive;
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
 }
 
-h1 {
-  color: #202124;
-  font-size: 22px;
-  line-height: 26px;
-  font-weight: 400;
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
 }
 
-ul {
-  margin: 0;
-  padding: 0;
-}
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
 
-li {
-  color: #000;
-  display: block;
-  font-size: 16px;
-  font-weight: 100;
-  line-height: 16px;
-  list-style: none;
-}
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
-.weatherApp {
-  border: 2px solid grey;
-  padding: 30px;
-  margin: 15px auto;
-  max-width: 620px;
-
-  background-image: url(../img/background.jpg);
-  background-size: cover;
-}
-}
-
-#fa-2 #fa-4 {
-  font-size: 60px;
-  font-weight: 400;
-  line-height: 5;
-}
-
-.weatherIcon {
-  height: 50px;
-  width: 50px;
-}
-
-.units {
-  position: relative;
-  top: -5px;
-}
-
-.searchForm .button-location{
-  margin-bottom: 15px;
-}
-
-.moreInfo {
-  taxt text-align: center;
-}
-
-span.nextDays {
-  font-weight: bold;
-  text-align: center;
-  font-size: 50px;
-}
-
-footer {
-  display: block;
-  text-align: center;
-  font-size: 20px;
-}
-
-.fa-gratipay {
-  color: red;
-}
+searchCity("Paris");  
